62cbfea66114de69cbfa0ff106dc439cdc670a4e
Goddie04 <139235915+Goddie04@users.noreply.github.com>
2023-10-10 06:37:30 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+// let x=60;
+// let f=2;
+//  let g=(x/f)
+
+//  document.write('the value of g is:' +g)
+
+// let note1= "vidflix is  confidential";
+// let note2 = " and a private owned company";
+
+// document.getElementById('demo').innerHTML= note1 +" "+ note2;
